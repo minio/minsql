3f7e723ff33a7040e6e5aa209ef8104ca37583ad
Sidhartha Mani <wlan0@users.noreply.github.com>
2019-03-11 11:47:18 -0700

    use go standard build tag, and remove go version checking library (#15)

--- go.mod
+++ go.mod
@@ -6,15 +6,21 @@ require (
 	github.com/BurntSushi/toml v0.3.1
 	github.com/alecthomas/participle v0.2.1 // indirect
 	github.com/dustin/go-humanize v1.0.0
+	github.com/go-ini/ini v1.42.0 // indirect
+	github.com/gopherjs/gopherjs v0.0.0-20181103185306-d547d1d9531e // indirect
 	github.com/gorilla/mux v1.7.0
-	github.com/hashicorp/go-version v1.1.0
 	github.com/minio/cli v1.3.0
-	github.com/minio/mc v0.0.0-20190304041834-1761b338f9f0
 	github.com/minio/minio v0.0.0-20190304223314-e8e9cd3e743b
 	github.com/minio/minio-go v6.0.14+incompatible
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/rakyll/statik v0.1.5
+	github.com/rjeczalik/notify v0.9.2 // indirect
+	github.com/smartystreets/assertions v0.0.0-20190116191733-b6c0e53d7304 // indirect
 	github.com/smartystreets/goconvey v0.0.0-20190222223459-a17d461953aa // indirect
+	github.com/stretchr/testify v1.3.0 // indirect
+	golang.org/x/crypto v0.0.0-20190131182504-b8fe1690c613 // indirect
 	golang.org/x/net v0.0.0-20190301231341-16b79f2e4e95
+	golang.org/x/sys v0.0.0-20190204203706-41f3e6584952 // indirect
+	golang.org/x/text v0.3.0 // indirect
 	gopkg.in/ini.v1 v1.42.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,3 +1,5 @@
+// +build go1.12
+
 /*
  * MinSQL, (C) 2019 MinIO, Inc.
  *
@@ -23,46 +25,11 @@
 package main // import "github.com/minio/minsql"
 
 import (
-	"fmt"
 	"os"
-	"runtime"
 
-	version "github.com/hashicorp/go-version"
-	"github.com/minio/mc/pkg/console"
 	"github.com/minio/minsql/server"
 )
 
-const (
-	// Minio requires at least Go v1.12
-	minGoVersion        = "1.12"
-	goVersionConstraint = ">= " + minGoVersion
-)
-
-// Check if this binary is compiled with at least minimum Go version.
-func checkGoVersion(goVersionStr string) error {
-	constraint, err := version.NewConstraint(goVersionConstraint)
-	if err != nil {
-		return fmt.Errorf("'%s': %s", goVersionConstraint, err)
-	}
-
-	goVersion, err := version.NewVersion(goVersionStr)
-	if err != nil {
-		return err
-	}
-
-	if !constraint.Check(goVersion) {
-		return fmt.Errorf("minsql is not compiled by go %s, minimum required version is %s. Please recompile accordingly", goVersionConstraint, minGoVersion)
-	}
-
-	return nil
-}
-
 func main() {
-	// When `go get` is used minimum Go version check is not triggered but it would have compiled it successfully.
-	// However such binary will fail at runtime, hence we also check Go version at runtime.
-	if err := checkGoVersion(runtime.Version()[2:]); err != nil {
-		console.Errorln(err)
-	}
-
 	server.Serve(os.Args)
 }
