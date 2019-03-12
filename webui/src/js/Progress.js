425625790695d0c9b57c95135c8e8cc5b8676575
Kanagaraj M <kanagaraj@minio.io>
2019-03-12 17:18:26 +0530

    improve query ui styling

--- webui/src/js/Progress.js
+++ webui/src/js/Progress.js
@@ -3,9 +3,9 @@ import filesize from "filesize"
 
 export const Progress = ({ size }) => {
   return (
-    <div className="notification progress__bar">
+    <div className="box progress__bar">
       Downloading {size > 0 && <span>({filesize(size)})</span>}
-      <progress className="progress is-primary" max="100">
+      <progress className="progress" max="100">
         15%
       </progress>
     </div>
