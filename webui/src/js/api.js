f0c6b955c8c49fbe0a51d30a53f2aff46267faa2
Kanagaraj M <kanagaraj@minio.io>
2019-03-20 18:13:13 +0530

    fix error handling for streaming data in ui

--- webui/src/js/api.js
+++ webui/src/js/api.js
@@ -10,28 +10,54 @@ export class API {
         body: sql
       })
         .then(res => {
+          if (!res.ok) {
+            res.text().then(reject)
+            return
+          }
           var decoder = new TextDecoder()
           var reader = res.body.getReader()
           var data = []
-          reader.read().then(function readData(result) {
-            if (result.value === undefined) {
-              reject("There are no results")
-              return
-            }
 
-            const value = decoder.decode(result.value, { stream: true })
-            const lines = value.split("\n")
-            for (let line of lines) {
-              try {
-                const parsed = JSON.parse(line)
-                data.push(parsed)
-              } catch (err) {
+          function readData() {
+            reader
+              .read()
+              .then(result => {
+                if (result.value !== undefined) {
+                  const value = decoder.decode(result.value, { stream: true })
+                  const lines = value.split("\n").filter(l => l.length > 0)
+                  for (let line of lines) {
+                    try {
+                      const parsed = JSON.parse(line)
+                      data.push(parsed)
+                    } catch (err) {
+                      console.log(err)
+                    }
+                  }
+
+                  // We will read at most 100 records
+                  if (data.length >= 100) {
+                    reader.cancel()
+                    resolve(data)
+                  }
+                }
+
+                // If we have reached the end, close the reader and return(resolve) the data
+                if (result.done) {
+                  reader.cancel()
+                  resolve(data)
+                } else {
+                  // Continue reading
+                  readData()
+                }
+              })
+              .catch(err => {
                 console.log(err)
-              }
-            }
-            resolve(data)
-            reader.cancel()
-          })
+                reject(err)
+              })
+          }
+
+          // Start reading data
+          readData()
         })
         .catch(err => {
           console.log(err)
