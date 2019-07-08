8252a47f2de0b3755fd37bd8f6456825a92fc570
Daniel Valdivia <hola@danielvaldivia.com>
2019-07-08 09:59:51 -0700

    Remove Lib change

--- src/lib.rs
+++ src/lib.rs
@@ -13,9 +13,6 @@
 //
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#[allow(unused)]
-#[macro_use]
-extern crate bitflags;
 
 use std::collections::HashMap;
 use std::fs::File;
